a65e90123e3bba0bacbd6731b9dc400841d2fd0f
Viloren88 <garifullin.marat@gmail.com>
2022-03-30 22:13:45 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,12 +1,8 @@
 package ru.job4j.chess.firuges.black;
 
 import org.junit.Test;
-import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
-import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
-import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
